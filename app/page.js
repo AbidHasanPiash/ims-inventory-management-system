63d358797164d3a1fa398feec22cf6da1e2e4152
Abid <meghpiash@gmail.com>
2023-05-10 19:09:40 +0600

    letup sidebar layout

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <div className="flex w-full h-full items-center justify-center border">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1>Dashboard</h1>
+        <h1 className="text-5xl">Dashboard</h1>
       </div>
-    </main>
+    </div>
   );
 }
