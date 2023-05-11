fb7dfe978a3358b340100d0e2f0dc8f3c367457d
Abid <meghpiash@gmail.com>
2023-05-11 12:06:56 +0600

    sidebar is colapsable now

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -7,34 +7,39 @@ export default function SideBar() {
   const {isSidebarOpen} = useContext(SidebarContext);
   return (
     <aside className={`${isSidebarOpen?'w-20':'w-72'} duration-300 min-h-screen px-2 bg-secondary text-primary text-xl`}>
-        <div className='h-14 px-3 border-b border-primary border-opacity-50 flex items-center'>
-            <div className='h-8 w-8 rounded-full bg-primary text-secondary font-bold text-center flex items-center justify-center'>
-                <span>P</span>
-            </div>
-            <h1 className='pl-2'>ABID-IMS</h1>
+      {/* -----------Brand Name----------- */}
+      <div className='h-14 px-3 border-b border-primary border-opacity-50 flex items-center'>
+          <div className='h-8 w-8 rounded-full bg-primary text-secondary font-bold text-center flex items-center justify-center'>
+              <span>P</span>
+          </div>
+          <h1 className={`${isSidebarOpen ? 'text-[0px]':'block'} pl-2 duration-300`}>ABID-IMS</h1>
+      </div>
+      {/* -----------User Name----------- */}
+      <div className='h-16 px-3 border-b border-primary border-opacity-50 flex items-center'>
+          <div className='h-8 w-8 rounded-full bg-primary text-secondary font-bold text-center flex items-center justify-center'>
+              <span>P</span>
+          </div>
+          <h1 className={`${isSidebarOpen ? 'text-[0px]':'block'} pl-2 duration-300`}>User id</h1>
+      </div>
+      {/* -----------Search Section----------- */}
+      <div className={`flex items-center justify-start border border-primary border-opacity-50 my-3 rounded`}>
+        <input 
+          type="text" 
+          placeholder='Search' 
+          className={`${isSidebarOpen ? 'hidden':'block'} w-full h-9 bg-secondary-hover rounded-l outline-none pl-3 border-r border-primary border-opacity-50 focus:ring-1`}/>
+        <div className='w-14 h-9 flex items-center justify-center'>
+          <RiSearch2Line />
         </div>
-        <div className='h-16 px-3 border-b border-primary border-opacity-50 flex items-center'>
-            <div className='h-8 w-8 rounded-full bg-primary text-secondary font-bold text-center flex items-center justify-center'>
-                <span>P</span>
-            </div>
-            <h1 className='pl-2'>User id</h1>
-        </div>
-        <div className={`duration-300 flex items-center justify-start border border-primary border-opacity-50 my-3 rounded`}>
-          <input 
-            type="text" 
-            placeholder='Search' 
-            className='w-full h-9 bg-secondary-hover rounded-l outline-none pl-3 border-r border-primary border-opacity-50 focus:ring-1'/>
-          <RiSearch2Line className='w-14 text-white '/>
-        </div>
-        <div className='text-lg'>
-            <ul>
-                <li className='h-10 my-2 flex items-center px-2 rounded hover:bg-secondary-hover'>
-                    <span className='w-8 flex items-center justify-center'><RxDashboard/></span>
-                    <span>Dashboard</span>
-                </li>
-            </ul>
-            <span>{isSidebarOpen ? 'open' : 'close'}</span>
-        </div>
+      </div>
+      {/* -----------Sidebar Menu Items----------- */}
+      <div className='text-lg'>
+          <ul>
+              <li className='h-10 my-2 flex items-center px-2 rounded hover:bg-secondary-hover'>
+                  <span className='w-8 flex items-center justify-center'><RxDashboard/></span>
+                  <span className={`${isSidebarOpen ? 'text-[0px]':'block'} pl-2 duration-300`}>Dashboard</span>
+              </li>
+          </ul>
+      </div>
     </aside>
   )
 }
